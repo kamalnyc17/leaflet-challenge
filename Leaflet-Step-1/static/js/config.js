417e47745ec873cce6ebbf0cf7186968dbd4df22
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-09-02 07:21:04 -0400

    initial setup for part one

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia2FtYWxueWMxNyIsImEiOiJja2VmbHp3MXUwNjRuMndtbjRlczg5bGdqIn0.hjkx0oBeYYPSbDDoKaocPw"
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,3 @@
+// main file to plot the geo map
+// url for weekly earthquake data
+const earthquakeURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'

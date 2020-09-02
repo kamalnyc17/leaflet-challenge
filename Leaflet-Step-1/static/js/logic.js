ea1bb8c5c24968560d39f91a30ecee63bae2d139
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-09-02 07:34:12 -0400

    plotting base map

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,3 +1,17 @@
 // main file to plot the geo map
 // url for weekly earthquake data
 const earthquakeURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+
+// Create a map object
+const myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
+  
+// setting up default gray scale map
+const grayScaleMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+}).addTo(myMap);

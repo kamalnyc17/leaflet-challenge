31043f23e9f4350156abb97424b0ac3ec6fc25a2
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-09-02 08:03:09 -0400

    fixed mapbox url

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -6,7 +6,7 @@ const earthquakeURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary
 const earthquakeLayer = new L.LayerGroup();
 
 // setting up default gray scale map
-const grayScaleMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+const grayScaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "light-v10",
@@ -26,7 +26,7 @@ var overlayMaps = {
 // Create map object and set default layers
 var myMap = L.map("map", {
   center: [37.09, -95.71],
-  zoom: 6,
+  zoom: 5,
   layers: [grayScaleMap, earthquakeLayer]
 });
 

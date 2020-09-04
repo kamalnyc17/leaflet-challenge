1e15c956368ba8f06b4fd5d63d0515c1fad32015
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-09-04 10:11:50 -0400

    cleaning up folder part 3

--- Leaflet-Step-1/static/js/config.js
+++
@@ -1,5 +0,0 @@
-// API key public
-//const API_KEY = "pk.eyJ1Ijoia2FtYWxueWMxNyIsImEiOiJja2VmbHp3MXUwNjRuMndtbjRlczg5bGdqIn0.hjkx0oBeYYPSbDDoKaocPw"
-
-// API key project specific
-const API_KEY = "pk.eyJ1Ijoia2FtYWxueWMxNyIsImEiOiJja2VmbTNhYWcwY25sMnByenRnOHdmNG14In0.uOjkhsw3c8KsqeMOGBFWGA"
